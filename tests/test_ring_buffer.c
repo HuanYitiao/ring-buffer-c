af58b0aee7c0eb26a76e352fd6ad0c5da8dfa96d
Qinghuan Liu <qinghuan.liu@globetech.com>
2026-04-28 14:06:26 +0200

    test: add test_rb_push_reject_when_full

--- tests/test_ring_buffer.c
+++ tests/test_ring_buffer.c
@@ -1,10 +1,15 @@
 #include "ring_buffer.h"
 #include "unity.h"
 
-static RingBuffer rb;
-static uint8_t buf[8];
+#define BUFFER_CAPACITY 8
 
-void setUp(void) { rb_init(&rb, buf, 8, RB_MODE_REJECT); }
+static RingBuffer rb;
+static RingBuffer rb_reject;
+static RingBuffer rb_overwrite;
+static uint8_t buf[BUFFER_CAPACITY];
+static uint8_t buf_reject[BUFFER_CAPACITY];
+
+void setUp(void) { rb_init(&rb, buf, BUFFER_CAPACITY, RB_MODE_REJECT); }
 
 void tearDown(void) { rb_clear(&rb); }
 
@@ -19,9 +24,18 @@ void test_rb_push(void) {
   TEST_ASSERT_FALSE(rb_is_empty(&rb));
 }
 
+void test_rb_push_reject_when_full(void) {
+  rb_init(&rb_reject, buf_reject, BUFFER_CAPACITY, RB_MODE_REJECT);
+  for (int i = 0; i < BUFFER_CAPACITY; i++) {
+    rb_push(&rb_reject, 1);
+  }
+  TEST_ASSERT_FALSE(rb_push(&rb_reject, 2));
+}
+
 int main(void) {
   UNITY_BEGIN();
   RUN_TEST(test_init_state);
   RUN_TEST(test_rb_push);
+  RUN_TEST(test_rb_push_reject_when_full);
   return UNITY_END();
 }
